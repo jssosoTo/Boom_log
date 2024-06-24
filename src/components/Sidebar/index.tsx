80fe3c1646bfbd3ff178820d8313e2cc4fa94c87
P0PC0RN <2982538245@qq.com>
2024-06-24 13:06:14 +0800

    ✨ feat(全局): Sidebar组件新增回退页按钮

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,16 +1,20 @@
 import classNames from "classnames";
 import styles from "./index.module.css";
 import { ReactNode, useState } from "react";
-import { MenuOutlined } from "@ant-design/icons";
+import { MenuOutlined, RollbackOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 function Sidebar({
   children,
   sidebarContent,
+  isWithdraw,
 }: {
   children: ReactNode;
   sidebarContent: ReactNode;
+  isWithdraw?: boolean;
 }) {
   const [isHide, setIsHide] = useState<boolean>(false);
+  const navigate = useNavigate();
 
   return (
     <div className="flex relative">
@@ -20,9 +24,18 @@ function Sidebar({
         {sidebarContent}
       </div>
       {children}
-      <button className={styles.MenuBtn} onClick={() => setIsHide(!isHide)}>
-        <MenuOutlined />
-      </button>
+      <div className={styles.MenuBtns}>
+        <button className={styles.MenuBtn} onClick={() => setIsHide(!isHide)}>
+          <MenuOutlined />
+        </button>
+        <button
+          hidden={!isWithdraw}
+          className={styles.WithdrawBtn}
+          onClick={() => navigate(-1)}
+        >
+          <RollbackOutlined />
+        </button>
+      </div>
     </div>
   );
 }
